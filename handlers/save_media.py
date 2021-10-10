d8673273bfc48c4b5df17ac1192e32ce312839ea
ThaniOruvan25 <89347874+ThaniOruvan25@users.noreply.github.com>
2021-10-10 09:41:21 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -48,9 +48,9 @@ async def SaveBatchMediaInChannel(bot: Client, editable: Message, message_ids: l
         )
         share_link = f"https://t.me/{Config.BOT_USERNAME}?start=ThaniOruvan25_{str_to_b64(str(SaveMessage.message_id))}**"
         await editable.edit(
-            f"**Video Link:**\n"
+            f"**Here is Sharable Link of this file:**\n"
             f"https://t.me/{Config.BOT_USERNAME}?start=ThaniOruvan_{str_to_b64(str(file_id))}\n\n"
-            f"**#TO UPLOADS💛**",
+            f"__To Retrive the Stored File, just open the link!__",
             parse_mode="Markdown",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Open The Link", url=share_link)],
